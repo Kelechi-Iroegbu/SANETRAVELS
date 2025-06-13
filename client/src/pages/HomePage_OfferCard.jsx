a6894cb20934a291c327955ac81decdc29225364
remilekun-elijah <remilekunelijah21997@gmail.com>
2025-06-13 13:00:29 +0100

    optimize codebase and restructure folder

--- client/src/pages/HomePage_OfferCard.jsx
+++ client/src/pages/HomePage_OfferCard.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function HomePage_OfferCard(props) {
  
- console.log(props.data)
- 
     return (
     <div className=' inline-block p-4  w-[181px] h-[219px] t-[477px] left-[190px] mt-9 border-none  '>
             <img src={props.data.logoUrl}/>
---
+++ client/src/pages/LogoCard.jsx
@@ -0,0 +1,7 @@
+export default function logoCard(props) {
+  return (
+    <div className=" inline-block p-4  w-[1440px] h-[100px] t-[1001px] bg-[#F7F7F7]">
+      <img src={props.data.logo} />
+    </div>
+  );
+}
