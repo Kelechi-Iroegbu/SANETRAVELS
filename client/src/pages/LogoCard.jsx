e4dd49adc54d27174df1bb9db35048c895aa701c
kelly <kiroegbu@gmail.com>
2025-06-14 03:16:25 +0100

    changes

--- client/src/pages/LogoCard.jsx
+++ client/src/pages/LogoCard.jsx
@@ -1,4 +1,4 @@
-export default function logoCard(props) {
+export default function logo_Card(props) {
   return (
     <div className=" inline-block p-4  w-[1440px] h-[100px] t-[1001px] bg-[#F7F7F7]">
       <img src={props.data.logo} />
