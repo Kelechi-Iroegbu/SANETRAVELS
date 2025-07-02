5cff6275b983d90a020dfbf018c9ffe6217d4f2f
remilekun-elijah <remilekunelijah21997@gmail.com>
2025-07-02 14:39:42 +0100

    update grid layout

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,66 +1,65 @@
-import logo from "./logo.svg"
-import facebook from "./facebook.svg"
-import instagram from "./instagram.svg"
-import twitter from "./twitter.svg"
-import home from "./Frame 90.svg"
-import city from "./City.svg"
-import mountain from "./mountain.jpg"
-import world_vector from "./world_vector.svg"
-import deco_text from "./deco_text.svg"
-import bg_style from "./bg_style.svg"
-import Ellipse from "./Ellipse.svg"
-import Image from "./Image.svg"
-import video from "./video.svg"
-import Vector_map from "./Vector_map.svg"
-import Mask_group from "./Mask_group.svg"
-import second_map from "./second_map.svg"
-import blur from "./blur.svg"
-import small_ellipse from "./small_ellipse.svg"
-import Big_ellipse from "./Big_ellipse.svg"
-import Boat from "./Boat.svg"
-import Grouped_text from "./Grouped_text.svg"
-import Group_text2 from "./Group_text2.svg"
-import bg_last from "./bg_last.svg"
-import vector_eco from "./vector_eco.svg"
-import flight_landing from "./flight_landing.svg"
-import flight_takeoff from "./flight_takeoff.svg"
-import direction from "./direction.svg"
-import solar_calender from "./solar_calendar.svg"
-import people from "./people.svg"
-import map_booking from "./map_booking.svg"
+import bg_last from "./bg_last.svg";
+import bg_style from "./bg_style.svg";
+import Big_ellipse from "./Big_ellipse.svg";
+import blur from "./blur.svg";
+import Boat from "./Boat.svg";
+import bookingBackground from "./bookingBackground.svg";
+import city from "./City.svg";
+import deco_text from "./deco_text.svg";
+import direction from "./direction.svg";
+import Ellipse from "./Ellipse.svg";
+import facebook from "./facebook.svg";
+import flight_landing from "./flight_landing.svg";
+import flight_takeoff from "./flight_takeoff.svg";
+import home from "./Frame 90.svg";
+import Group_text2 from "./Group_text2.svg";
+import Grouped_text from "./Grouped_text.svg";
+import Image from "./Image.svg";
+import instagram from "./instagram.svg";
+import logo from "./logo.svg";
+import map_booking from "./map_booking.svg";
+import Mask_group from "./Mask_group.svg";
+import mountain from "./mountain.jpg";
+import people from "./people.svg";
+import second_map from "./second_map.svg";
+import small_ellipse from "./small_ellipse.svg";
+import solar_calender from "./solar_calendar.svg";
+import twitter from "./twitter.svg";
+import vector_eco from "./vector_eco.svg";
+import Vector_map from "./Vector_map.svg";
+import video from "./video.svg";
+import world_vector from "./world_vector.svg";
 
-
-
-
-export const assets  = {
-    logo,
-    facebook,
-    instagram,
-    twitter,
-    home,
-    city,
-    mountain,
-    world_vector,
-    deco_text,
-    bg_style,
-    Ellipse,
-    Image,
-    video,
-    Vector_map,
-    Mask_group,
-    second_map,
-    blur,
-    small_ellipse,
-    Big_ellipse,
-    Boat,
-    Grouped_text,
-    Group_text2,
-    bg_last,
-    vector_eco,
-    flight_landing,
-    flight_takeoff,
-    direction,
-    solar_calender,
-    people,
-    map_booking,
-}
+export const assets = {
+  logo,
+  facebook,
+  instagram,
+  twitter,
+  home,
+  city,
+  mountain,
+  world_vector,
+  deco_text,
+  bg_style,
+  Ellipse,
+  Image,
+  video,
+  Vector_map,
+  Mask_group,
+  second_map,
+  blur,
+  small_ellipse,
+  Big_ellipse,
+  Boat,
+  Grouped_text,
+  Group_text2,
+  bg_last,
+  vector_eco,
+  flight_landing,
+  flight_takeoff,
+  direction,
+  solar_calender,
+  people,
+  map_booking,
+  bookingBackground,
+};
--- client/src/components/GridLayout.jsx
+++ client/src/components/GridLayout.jsx
@@ -1,40 +1,159 @@
-import React from 'react';
-import { Grid } from '@mui/material';
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
 
 const GridLayout = () => {
-  const columns = 6;
+  const scrollContainerRef = useRef(null); // Ref with HTMLDivElement type
+  const columns = 30;
   const firstRowHeight = 112;
   const otherRowsHeight = 72;
   const boxWidth = 141.67;
 
-  return (
-    <div className="w-full">
-      {/* First Row */}
-      <Grid container>
-      
-        {[...Array(columns)].map((_, idx) => (
-          <Grid item key={`row1-col${idx}`} xs>
-            <div
-              style={{ width: `${boxWidth}px`, height: `${firstRowHeight}px` }}
-              className="border bg-white"
-            />
-          </Grid>
-        ))}
-      </Grid>
+  const handleNextClick = () => {
+    if (scrollContainerRef.current) {
+      const container = scrollContainerRef.current;
+      const containerWidth = container.clientWidth;
+      const itemWidth = container?.firstElementChild?.clientWidth; // Added null check for firstElementChild
 
-      {/* Other Rows (3 rows) */}
-      {[...Array(3)].map((_, rowIndex) => (
+      if (itemWidth !== undefined) {
+        // Added check for itemWidth
+        container.scrollBy({
+          left: containerWidth - itemWidth, // the last item becomes the first item in the new sets after scroll
+          behavior: "smooth",
+        });
+      }
+    }
+  };
+
+  // Function to scroll to the previous item
+  const handlePrevClick = () => {
+    if (scrollContainerRef.current) {
+      const container = scrollContainerRef.current;
+      const containerWidth = container.clientWidth;
+      const itemWidth = container?.firstElementChild?.clientWidth; // Added null check for firstElementChild
+
+      if (itemWidth) {
+        // Added check for itemWidth
+        container.scrollBy({
+          left: -(containerWidth - itemWidth), // the first item becomes the last item in the new sets after scroll
+          behavior: "smooth",
+        });
+      }
+    }
+  };
+
+  // Function to scroll the clicked item into view
+  const scrollToItem = (index) => {
+    // setSelected(services[index]);
+    reset();
+
+    if (scrollContainerRef.current) {
+      const container = scrollContainerRef.current;
+      const item = container.children[index];
+      if (item) {
+        item.scrollIntoView({
+          behavior: "smooth",
+          block: "nearest",
+          inline: "center",
+        });
+      }
+    }
+  };
+
+  const [showPrev, setShowPrev] = useState(false); // Control visibility of "Previous" button
+  const [showNext, setShowNext] = useState(true); // Control visibility of "Next" button
+
+  // Scroll event listener to show/hide buttons
+  const handleScroll = () => {
+    if (scrollContainerRef.current) {
+      const container = scrollContainerRef?.current;
+      const offset = container?.firstElementChild?.clientWidth || 0;
+      const maxScrollLeft = container.scrollWidth - container.clientWidth;
+      const scrollLeft = container.scrollLeft;
+
+      setShowPrev(scrollLeft > offset); // Show "Previous" if scroll is not at the start
+      setShowNext(scrollLeft + 8 < maxScrollLeft); // Show "Next" if scroll is not at the end
+    }
+  };
+
+  // Add scroll event listener
+  useEffect(() => {
+    const container = scrollContainerRef.current;
+    if (container) {
+      container.addEventListener("scroll", handleScroll);
+      handleScroll(); // Initial check for buttons visibility
+    }
+    return () => {
+      if (container) {
+        container.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
+
+  return (
+    <div className="relative flex gap-5 mt-5 max-w-[1000px]">
+      {/* {showPrev && ( */}
+      <div
+        onClick={handlePrevClick}
+        className="absolute shadow mt-2 z-50 right-2 top-10 border flex items-center rounded h-[33px] cursor-pointer hover:opacity-50"
+      >
+        {/* <KeyboardArrowLeftIcon /> */}
+        <ChevronRight className=" border rounded-full text-white bg-red-400 hover:bg-red-500 transition duration-300 cursor-pointer" />
+      </div>
+      {/* )} */}
+      <div
+        className="--scroll gap-5 overflow-x-scroll flex p-2"
+        ref={scrollContainerRef}
+        style={{
+          scrollSnapType: "x mandatory",
+          scrollBehavior: "smooth",
+        }}
+      >
+        {/* <div className="w-full relative"> */}
+        {/* First Row */}
+        {/* <div className="flex gap-5  w-full"> */}
+        {[...Array(columns)].map((_, idx) => (
+          <div className="">
+            <div
+              onClick={() => scrollToItem(idx)}
+              style={{
+                scrollSnapAlign: "start",
+                width: `${boxWidth}px`,
+                height: `${firstRowHeight}px`,
+              }}
+              className="border bg-white flex justify-center items-center"
+            >
+              {idx + 1}
+            </div>
+          </div>
+        ))}
+        {/* </div> */}
+
+        {/* Other Rows (3 rows) */}
+        {/* {[...Array(2)].map((_, rowIndex) => (
         <Grid container key={`row-${rowIndex + 2}`}>
           {[...Array(columns)].map((_, colIndex) => (
             <Grid item key={`row${rowIndex + 2}-col${colIndex}`} xs>
               <div
-                style={{ width: `${boxWidth}px`, height: `${otherRowsHeight}px` }}
+                style={{
+                  width: `${boxWidth}px`,
+                  height: `${otherRowsHeight}px`,
+                }}
                 className="border bg-white"
               />
             </Grid>
           ))}
         </Grid>
-      ))}
+      ))} */}
+      </div>
+      {/* {showNext && ( */}
+      <div
+        onClick={handleNextClick}
+        className="absolute left-2 top-10 z-50 shadow mt-2 border flex items-center rounded h-[33px] cursor-pointer hover:opacity-50"
+      >
+        <ChevronLeft className="border rounded-full text-white bg-red-400 hover:bg-red-500 transition duration-300 cursor-pointer" />
+        {/* <KeyboardArrowRightIcon /> */}
+      </div>
+      {/* )} */}
     </div>
   );
 };
